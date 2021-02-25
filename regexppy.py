4cf6e4e921a0ec3a91a65256b8e88e94e13ee970
unknown <nikhilsunil90s@gmail.com>
2021-02-25 09:43:11 +0530

    Regex Done

--- regexppy.py
+++ regexppy.py
@@ -40,3 +40,36 @@ st = 'An Amazing module!'
 #pattern = '^An'
 pattern = 'ule[.]$'
 print(re.search(pattern, st))
+
+# Repeat Metachars
+# * ---- it can come either 0 time or any no. of times
+# + ---- can come 1 or any no. times
+# ? ---- can come 0 or 1 time
+# {min,max}
+st = 'this string is in brown colouuuur'
+pattern = 'colo(u){1,3}r'
+print(re.search(pattern , st))
+
+# Character Classes ----- no metachar holds its functionality inside character class
+# ^ ---- to invert a class , but only in the begining
+pattern = '[A-Z]'
+pattern = '[a-z]'
+pattern = '[0-9]'
+pattern = '[A-Za-z0-9]'
+pattern = '[p-y]'
+pattern = '[FRAHJKLOP]'
+pattern = '[AEIOUHY]'
+pattern = '[^$#.%&^+?*}{]'
+st = 'hsdgfkjshdgfkshgdfksghdfkRsdf0'
+st = '$#.%&^+?*}{a'
+print(re.search(pattern, st))
+
+# Symbols
+# \w --->  [A-Za-z0-9]
+# \W ---> [0-9]
+# \d ---> digits
+# \s ---> special chars
+# \S ---> white space
+# sample09@email.com
+email = '\w{8,12}@{1}(gmail|yahoo|rediff|email)(?=.com)'# follows # raw string
+print(re.findall(email, 'rediff12@email.com'))
